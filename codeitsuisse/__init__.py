62623ee808d2f65a62db29f83319ec94f73e7f6b
Xinyi Xu <xinyi.xu97@gmail.com>
2021-09-26 02:35:48 +0100

    Move back hunter

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
-#import codeitsuisse.routes.stock_hunter
+import codeitsuisse.routes.stock_hunter
 import codeitsuisse.routes.parasite
 import codeitsuisse.routes.asteroid
